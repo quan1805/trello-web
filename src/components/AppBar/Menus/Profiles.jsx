3206b80ab144a54d62045cdf7a993a548b0c105a
Quan <quan.di1207@gmail.com>
2024-04-12 15:29:29 +0700

    Create board bar

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -32,7 +32,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 32, height: 32 }}
+            sx={{ width: 34, height: 34 }}
             alt='Trello'
             src='https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg'
           />
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,121 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Tooltip } from '@mui/material'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+
+const MENU_STYPES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYPES}
+          icon={<DashboardIcon />}
+          label="QuanDev"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYPES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYPES}
+          icon={<AddToDriveIcon />}
+          label="Add to Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYPES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYPES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant='outlined' startIcon={<PersonAddIcon/>}>Invite</Button>
+        <AvatarGroup
+          max={3}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg" />
+          </Tooltip>
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://lh3.googleusercontent.com/ogw/AF2bZyigxHdLamtAlX1TOeIljDsep2cn5sBvlIwjDiuMW3JQAbw=s32-c-mo" />
+          </Tooltip>
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg" />
+          </Tooltip>
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg" />
+          </Tooltip>
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg" />
+          </Tooltip>
+          <Tooltip title='quandev'>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
